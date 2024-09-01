4faa1e6fd14c175ec8ccdcdb4d324d2dad063b15
cvinni21 <83455059+cvinni21@users.noreply.github.com>
2024-09-01 11:26:08 -0300

    update da filtragem por data incompleta
    fiz a primeira parte da filtragem por data porém ainda está incompleta

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,38 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Categories from '../Categories'
 import Logo from '../logo'
 import './style.css'
 import { useNavigate } from 'react-router-dom'
+import { PatternFormat } from 'react-number-format'
 
 
 export default function Header() {
 
     const [textFilter, setTextFilter] = useState('')
+    const [startDate, setStartDate] = useState('')
+    const [endDate, setEndDate] = useState('')
     const navigate = useNavigate()
+
+    console.log({ startDate, endDate })
+    useEffect(() => {
+        if (startDate || endDate) {
+            let params = []
+            if (startDate) {
+                params.push(`startDate=${formatDateString(startDate)}`)
+            }
+            if (endDate) {
+                params.push(`endDate=${formatDateString(endDate, false)}`)
+            }
+            navigate({
+                search: `?${params.join('&')}`
+            })
+        } else {
+            navigate({
+                search: ``
+            })
+        }
+    }, [startDate, endDate])
+
 
     return (
         <div className='header'>
@@ -34,10 +58,29 @@ export default function Header() {
                 </button>
             </div>
             <div className='filters'>
-                <input placeholder='Data de inicio' className='input' />
-                <input placeholder='Data de fim' className='input' />
+                <PatternFormat
+                    format='##/##/####'
+                    mask='_'
+                    placeholder='Data de inicio'
+                    className='input'
+                    value={startDate}
+                    onChange={(e) => setStartDate(e.target.value)}
+                />
+                <PatternFormat
+                    format='##/##/####'
+                    mask='_'
+                    placeholder='Data de fim'
+                    className='input'
+                    value={endDate}
+                    onChange={(e) => setEndDate(e.target.value)}
+                />
                 <input placeholder='Ordenar por' className='input' />
             </div>
         </div>
     )
+}
+
+function formatDateString(str, isStart = true) {
+    const [day, month, year] = str.split('/')
+    return `${year}${month}${day}${isStart ? '000000' : '235959' }`
 }
